73f963161bc6cae10cb31f87d23d1bcf6a6f1395
Paul Borawski <paulborawski14@gmail.com>
2018-03-29 15:42:29 -0700

    form is ready on the front-end

--- src/containers/DatabaseTester.js
+++ src/containers/DatabaseTester.js
@@ -1,19 +1,36 @@
 import React, { Component } from 'react';
 
 class DatabaseTester extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: "",
+      age: ""
+    }
+  }
 
-  consoleLogValue(e) {
+  changeTheName = (e) => {
+    console.log(e.target.value);
+    this.setState({name: e.target.value})
+  }
+
+  changeTheAge = (e) => {
     console.log(e.target.value)
+    this.setState({age: e.target.value})
   }
 
   render() {
     return (
       <div>
-        Name:
-       <input type="text" onChange={ this.consoleLogValue }></input>
-        Age:
-       <input type="number" onChange={ this.consoleLogValue }></input>
-       <div></div>
+        <form onSubmit={this.handleSubmit}>
+          <label>Name:</label>
+            <input type="text" onChange={ this.changeTheName }></input>
+          <label>Age:</label>
+            <input type="number" onChange={ this.changeTheAge }></input>
+          <button>Send it</button>  
+        </form>
+        <div>{this.state.name}</div>
+        <div>{this.state.age}</div>
       </div>
     )
   }
