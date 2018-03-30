7353b2fd71572129c63f092b945db8d671a35f92
Paul Borawski <paulborawski14@gmail.com>
2018-03-30 07:59:24 -0700

    the button is working

--- src/containers/DisplayOnScreen.js
+++ src/containers/DisplayOnScreen.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
 
 class DisplayOnScreen extends Component {
+
+  getTheInfo = (e) => {
+    console.log('working button')
+  }
+
   render() {
     return (
       <div>
         <div>Hello</div>
         <div>Hello again</div>
+        <button onClick={this.getTheInfo}>DISPLAY THAT</button>
+        <div></div>
       </div>
     )
   }
