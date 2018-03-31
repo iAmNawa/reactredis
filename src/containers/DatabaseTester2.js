0fbd85854a25f9aac3a1dcd20f5333e2369263fe
Paul Borawski <paulborawski14@gmail.com>
2018-03-30 19:16:07 -0700

    iterating through an array of objects and adding a new property to each object

--- src/containers/DatabaseTester2.js
+++ src/containers/DatabaseTester2.js
@@ -12,7 +12,13 @@ class DatabaseTester2 extends Component {
   async componentDidMount() {
     const res = await fetch('http://localhost:34564/userlist');
     const data = await res.json();
+    console.log(data);
     var array =[];
+    for (var prop in data) {
+    data[prop].newprop = "newprop";
+      console.log(data[prop])
+    }
+    console.log(data)
     data.map(x => array.push(`Hello I am ${x.name} and I am ${x.age} years old. `))
     this.setState({one: array})
   }
